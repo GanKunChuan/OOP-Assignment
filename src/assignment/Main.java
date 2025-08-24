327ae7bc4249f1ec51382c52e55869a591e29495
Bob Gan <Bob Gan@LAPTOP-CF774SSM>
2025-08-25 00:26:24 +0800

    Test

--- src/assignment/Main.java
+++ src/assignment/Main.java
@@ -52,6 +52,7 @@ public class Main {
         int choice;
         String input;
         do{
+            System.out.println("
             System.out.println("\n  _______   ______     ______ ____    ____  ______  __       _______ ");
             System.out.println(" /  _____| /  __  \\   /      |\\   \\  /   / /      ||  |     |   ____|");
             System.out.println("|  |  __  |  |  |  | |  ,----' \\   \\/   / |  ,----'|  |     |  |__   ");
